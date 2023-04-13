5b7d7efd6fd7e7e15516d508bab0f1c4bc5673f3
PratyayChakraborty <pratyaychakraborty024@gmail.com>
2023-04-13 16:02:53 +0530

    nav update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./Styles/Homesyl.css"
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Navbar1 from './Components/Navbar/Navbar1';
+import Navbar2 from './Components/Navbar/Navbar2';
 import AllRoutes from './Pages/AllRoutes';
 import Footer from './Pages/Footer';
 
-
+import { Button, ButtonGroup,Wrap } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
-      <Navbar1/>
+      {/* <Navbar1/> */}
+      <Navbar2 />
       <AllRoutes/>
       <Footer/>
     </div>
